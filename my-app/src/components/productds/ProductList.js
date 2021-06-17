bdf285073068ec0da29231859f868121fc1ff26f
ariga914 <82427651+ariga914@users.noreply.github.com>
2021-06-17 18:17:31 +0900

    OMG

--- my-app/src/components/productds/ProductList.js
+++ my-app/src/components/productds/ProductList.js
@@ -5,31 +5,31 @@ import './ProductList.css'
 function ProductList(props) {
     return (
         <List>
-            <ProductItem>
+            <ProductItem
                 name={props.products[0].name}
                 price={props.products[0].price}
                 category={props.products[0].category}
-            </ProductItem>
-            <ProductItem>
+            ></ProductItem>
+            <ProductItem
                 name={props.products[1].name}
                 price={props.products[1].price}
                 category={props.products[1].category}
-            </ProductItem>
-            <ProductItem>
+            ></ProductItem>
+            <ProductItem
                 name={props.products[2].name}
                 price={props.products[2].price}
                 category={props.products[2].category}
-            </ProductItem>
-            <ProductItem>
+            ></ProductItem>
+            <ProductItem
                 name={props.products[3].name}
                 price={props.products[3].price}
                 category={props.products[3].category}
-            </ProductItem>
-            <ProductItem>
+            ></ProductItem>
+            <ProductItem
                 name={props.products[4].name}
                 price={props.products[4].price}
                 category={props.products[4].category}
-            </ProductItem>
+            ></ProductItem>
         </List>
     );
 }
